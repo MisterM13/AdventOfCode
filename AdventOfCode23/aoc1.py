6fb9982675ed398f5efe15127045741837b9534a
Matthias Mueller <matthias95@me.com>
2023-12-10 00:34:00 +0100

    made revision day1 without success...

--- AdventOfCode23/aoc1.py
+++ AdventOfCode23/aoc1.py
@@ -5,6 +5,7 @@ input = f.read()
 
 #input = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet"
 #input ="two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen"
+#input = "one\n7\nnineighthree"
 
 input = input.split("\n")
 
@@ -15,9 +16,30 @@ def getFirstDigit(strarr):
 
 def substituteTextNumbers(arr):
 	for i in range(0,len(arr)):
-		arr[i] = findFirstWrittenNum(arr[i])
+		#arr[i] = findFirstWrittenNum(arr[i])
+		arr[i] = substituteNums(arr[i])
 		#print("arr:",arr)
 	return arr
+
+def substituteNums(strarr):
+	numl = ["zero","one","two","three","four","five","six","seven","eight","nine"]
+	indArr = []
+	numArr = []
+	for j in range(1, len(numl)):
+		if(strarr.find(numl[j])>=0):
+			indArr.append(strarr.find(numl[j]))
+			numArr.append(j)
+	
+	subsArr = list(strarr)
+	for i in range(0,len(indArr)):
+		subsArr[indArr[i]] = str(numArr[i])
+	for s in range(0,len(subsArr)):
+		if not str(subsArr[s]).isdigit():
+			#print(subsArr[s])
+			subsArr[s] = ''
+			
+	return ''.join(subsArr)
+	
 
 def findFirstWrittenNum(strarr):
 	numl = ["zero","one","two","three","four","five","six","seven","eight","nine"]
@@ -27,9 +49,9 @@ def findFirstWrittenNum(strarr):
 		for j in range(0, len(numl)):
 			n = numl[j]
 			if(s.find(n)>=0):
-				print(n)
+				print(n,strarr)
 				strarr = strarr.replace(n, str(j))
-				break
+				break 
 	s = ""
 	for i in strarr[::-1]:
 		s= i+s
@@ -37,18 +59,33 @@ def findFirstWrittenNum(strarr):
 		for j in range(0, len(numl)):
 			n = numl[j]
 			if(s.find(n)>=0):
+				print(strarr,n)
 				strarr = strarr.replace(n, str(j))
-				break	
+				break 
 	print(strarr)
 	return strarr
 
+#c = 0
+#input = substituteTextNumbers(input)
+#print(input)
+#for i in input:
+#	a = getFirstDigit(i)
+#	b = getFirstDigit(i[::-1])
+#	#print(a,b)
+#	x = a+b
+#	print(x)
+#	c+=int(x)
+#print(c)
+
+#print(substituteNums("7srncljm5zmvvrtthreeninejjd85twonepvjeight"))
+#print(substituteNums("twoneighthreesevenine"))
+#print(substituteNums("onetwothreefourfivesixseveneightnine"))
+
 c = 0
 input = substituteTextNumbers(input)
-print(input)
 for i in input:
-	a = getFirstDigit(i)
-	b = getFirstDigit(i[::-1])
-	#print(a,b)
+	a = i[0]
+	b = i[len(i)-1]
 	x = a+b
 	print(x)
 	c+=int(x)
