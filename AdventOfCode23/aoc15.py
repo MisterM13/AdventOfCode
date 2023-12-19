e0d718729c641ead461601b23e21d5d5c108073b
Matthias Mueller <matthias95@me.com>
2023-12-19 08:20:50 +0100

    day 15

---
+++ AdventOfCode23/aoc15.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+f = open("input15.txt", "r")
+input = f.read()
+
+#input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
+#input = "HASH"
+
+initSeq = input.split(",")
+boxes = [[]]*256
+
+def hashString(str):
+	value = 0
+	for i in str:
+		value += ord(i)
+		value *= 17
+		value %= 256
+		#print(i,value)
+	return value
+
+def a():
+	sum = 0
+	for i in initSeq:
+		hash = hashString(i)
+		print(hash)
+		sum += hash
+	print(sum)
+	
+def boxOp(instr):
+	global boxes
+	replaced = False
+	if len(instr.split("=")) > 1:
+		#print("=")
+		x = instr.split("=")
+		y = " ".join(x)
+		boxnr = hashString(x[0])
+		for i in range(len(boxes[boxnr])):
+			if boxes[boxnr][i].startswith(x[0]):
+				#print(boxes[boxnr][i],y)
+				boxes[boxnr][i] = y
+				replaced = True
+		if not replaced:		
+			if len(boxes[boxnr])==0:
+				boxes[boxnr] = [y]
+			else:
+				boxes[boxnr].append(y)
+	else:
+		x = instr.split("-")[0]
+		boxnr = hashString(x)
+		for i in boxes[boxnr]:
+			if i.startswith(x[0]):
+				boxes[boxnr].remove(i)
+
+def focusPower(i):
+	global boxes
+	pow = 0
+	x = boxes[i]
+	for j in range(len(x)):
+		val = int(x[j].split(" ")[1])
+		pow += (i+1)*(j+1)*val
+	return pow
+
+
+def b():
+	sum = 0
+	for s in initSeq:
+		boxOp(s)
+	print(boxes)
+	for b in range(len(boxes)):
+		sum += focusPower(b)
+	print("Sum:",sum)
+	
+b()
